1e886ba92f54f8c08687c9104df22d190054ea81
管乐明 <1510086322@qq.com>
2021-06-17 11:49:07 +0800

    完成报表开发

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,8 @@ import Params from '@/components/goods/params.vue'
 import List from '@/components/goods/List.vue'
 import Add from '@/components/goods/Add.vue'
 import Order from '@/components/order/Order.vue'
+import Report from '@/components/repport/Report.vue'
+
 
 Vue.use(VueRouter)
 
@@ -57,6 +59,9 @@ const routes = [{
         }, {
             path: '/orders',
             component: Order
+        }, {
+            path: '/reports',
+            component: Report
         }]
     },
 
